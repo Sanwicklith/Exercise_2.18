6dab4097281a4ff89ec1a10c0eee9310cfed8c7b
Your Name <Sanwicklith>
2024-08-02 20:24:41 +0200

    exercise 2.18 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import getCountries from './services/countries';
+import Countries from './components/countries';
+import Country from './components/country';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countryList, setCountryList] = useState([]);
+  const [countryFilter, setCountryFilter] = useState('');
+
+  const url = 'https://studies.cs.helsinki.fi/restcountries/api/all';
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const objArray = await getCountries(url);
+      setCountryList(objArray);
+    };
+    fetchCountries();
+  }, []);
+
+  const filteredCountries = countryList.filter(country => {
+    return country.name.common.toLowerCase().includes(countryFilter.toLowerCase());
+  });
+
+  const handleChange = e => {
+    setCountryFilter(e.target.value);
+  };
+
+  const display = () => {
+    console.log(filteredCountries.length);
+    switch (filteredCountries.length) {
+      case 0:
+        return <p>No countries found</p>;
+      case 1:
+        return <Country countries={filteredCountries} />;
+
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+        return <Countries countries={filteredCountries} />;
+      default:
+        return <p>Too many matches, specify another filter</p>;
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Find Countries: <input onChange={handleChange} value={countryFilter} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{display()}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
