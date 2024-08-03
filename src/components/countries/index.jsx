c1e8eb7874246604c2c03e2be2c57adb99316c6d
Your Name <Sanwicklith>
2024-08-03 23:48:26 +0200

    exercise 2.19 complete

--- src/components/countries/index.jsx
+++ src/components/countries/index.jsx
@@ -1,9 +1,9 @@
 const Countries = ({ countries, handleClick }) => {
   return countries.map((country, i) => {
     return (
-      <div key={i} style={{display:'flex'}} >
+      <div key={i} style={{display:'flex', alignItems:"center"}} >
         <p >{country.name.common}</p>
-        <button onClick={()=> handleClick(country.name.common)}>Show</button>
+        <button style={{width:'60px', height:'35px', fontSize:'12px', margin:'10px', alignContent:'center'}} onClick={()=> handleClick(country.name.common)}>Show</button>
       </div>
     );
   });
